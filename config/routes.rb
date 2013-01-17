6d2ad9b86fe551967dd9c6221a1364f0d7aa135f
robotzhang <445679586@qq.com>
2013-01-17 20:03:18 +0800

    不用resources :albums 居然就找到albums/seo_url

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Tuku::Application.routes.draw do
   root :to => 'application#homepage'
   get 'albums/:seo_url' => 'albums#show'
+  resources :albums
   match 'login' => 'users#login'
   match 'logout' => 'users#destroy'
   match 'signup' => 'users#new'
